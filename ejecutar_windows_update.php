1f566b19fceda48c663017afe00b6caebcf5cb83
Ubuntu <ubuntu@ip-10-0-8-21.ec2.internal>
2025-05-14 10:41:38 +0000

    new function

--- ejecutar_windows_update.php
+++ ejecutar_windows_update.php
@@ -1,6 +1,4 @@
 <?php
-// ejecutar_accion.php
-
 // Recoger los datos enviados por POST
 $ip = escapeshellarg($_POST['ip']);
 $user = escapeshellarg($_POST['user']);
@@ -13,8 +11,6 @@ $command = "ansible-playbook /var/www/html/proyecto/playbooks/windows_update.yml
          . "ansible_connection=ssh ansible_shell_type=powershell "
          . "ansible_ssh_common_args='-o StrictHostKeyChecking=no'\"";
 
-         echo $command ;
-
 // Configurar descriptores para capturar stdout y stderr
 $descriptorspec = [
     1 => ["pipe", "w"], // stdout
@@ -26,16 +22,22 @@ $process = proc_open($command, $descriptorspec, $pipes);
 
 echo "<pre>";
 
+// Function to clean up output
+function cleanOutput($line) {
+    // Remove carriage returns (\r), newlines (\n), extra spaces, and trim
+    return trim(preg_replace('/[\r\n]+/', ' ', $line));
+}
+
 if (is_resource($process)) {
     // Leer salida estándar
     while ($line = fgets($pipes[1])) {
-        echo htmlspecialchars($line);
+        echo htmlspecialchars(cleanOutput($line)) . "<br>";  // Clean output before echoing
     }
 
     // Leer errores
     $stderr_output = stream_get_contents($pipes[2]);
     if (!empty($stderr_output)) {
-        echo "\n\n--- ERRORES DETECTADOS ---\n";
+        echo "<br><br>--- ERRORES DETECTADOS ---<br>";
         echo htmlspecialchars($stderr_output);
     }
 
@@ -45,8 +47,8 @@ if (is_resource($process)) {
 
     // Obtener el código de salida
     $return_value = proc_close($process);
-    echo "\n\n--- EJECUCIÓN FINALIZADA ---\n";
-    echo "Código de salida: $return_value\n";
+    echo "<br><br>--- EJECUCIÓN FINALIZADA ---<br>";
+    echo "Código de salida: $return_value<br>";
     if ($return_value !== 0) {
         echo "Hubo errores durante la ejecución del playbook.";
     } else {
